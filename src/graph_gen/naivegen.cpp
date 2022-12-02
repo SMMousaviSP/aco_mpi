71b7ef990154bee8926b386970fd0e2f4a6a26d5
SMMousaviSP <smmousavisp@gmail.com>
2022-12-02 18:43:35 +0100

    Adding NULL_TIE macro for unreachable nodes

--- src/graph_gen/naivegen.cpp
+++ src/graph_gen/naivegen.cpp
@@ -1,6 +1,8 @@
 #include <cstdlib>
 #include <stdexcept>
 
+#include "../settings.h"
+
 
 /**
  * @brief Generate weigthed graph where each tie is randomly generated.
@@ -14,7 +16,7 @@
  * @param seed      random generation
  * @return float**  adjacency matrix
  */
-float ** generate_naive_graph(int size, float min, float max, int seed) {
+float ** generateNaiveGraph(int size, float min, float max, int seed) {
 
     // Throw an exception if min is greater than max
     if (min > max) {
@@ -27,7 +29,7 @@ float ** generate_naive_graph(int size, float min, float max, int seed) {
         graph[i] = new float[size];
         for (int j = 0; j < size; j++) {
             if (i == j) {
-                graph[i][j] = 0;
+                graph[i][j] = NULL_TIE;
             } else {
                 graph[i][j] = (((float)rand() / (float)(RAND_MAX)) * (max - min)) + min;
             }
---
+++ src/settings.h
@@ -0,0 +1,1 @@
+#define NULL_TIE -1
