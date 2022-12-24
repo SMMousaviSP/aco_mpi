a014579f8cd4f098458702fa927120ccda407c9b
SMMousaviSP <smmousavisp@gmail.com>
2022-12-25 00:58:45 +0100

    Commenting extra stuff and running the ACO for TSP

--- src/acompi.cpp
+++ src/acompi.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#include "settings.h"
+#include "structs.h"
+#include "utility.cpp"
 #include "graph_gen/naivegen.cpp"
 #include "ant.cpp"
 #include "pheromone.cpp"
@@ -13,92 +16,97 @@ const float alpha = 0.5;
 const float beta = 0.5;
 
 int main() {
+
+    srand(time(NULL));
+
     int graphSize = SIZE;
     T_GRAPH** graphData;
     T_PHER** pheromones;
-    graphData = generateNaiveGraph(graphSize, 1.0, 6.0, SEED);
+    graphData = generateNaiveGraph(graphSize, 1.0, 6.0);
     pheromones = generatePheromones(graphSize, 1.0);
 
-    vector<int> neighbors = getNeighbors(2, graphData, graphSize);
-    vector<int> alreadyVisited;
-    alreadyVisited.push_back(1);
-    alreadyVisited.push_back(3);
-    vector<int> neighborsCleaned = eliminateAlreadyVisitedNeighbors(neighbors, alreadyVisited);
-
-    // print neighbors vector  
-    cout << "neighbors: " << endl;
-    for (int i = 0; i < neighbors.size(); i++) {
-        cout << neighbors[i] << endl;
-    }
-    cout << endl;
-
-
-    // print neighborsCleaned vector
-    cout << "cleaned neighbors: " << endl;
-    for (int i = 0; i < neighborsCleaned.size(); i++) {
-        cout << neighborsCleaned[i] << endl;
-    }
-    cout << endl;
-
-
-    // print graphData
-    cout << "graph: " << endl;
-    for (int i = 0; i < graphSize; i++) {
-        for (int j = 0; j < graphSize; j++) {
-            cout << graphData[i][j] << " ";
+    // Running the Ants
+    AntPath antPathArray[ANTS_N];
+    for (int i = 0; i < ANTS_ITER; i++) {
+        for (int i = 0; i < ANTS_N; i++) {
+            // Choose a random starting node from 0 to SIZE
+            int startingNode = rand() % SIZE;
+            antPathArray[i] = antRun(startingNode, graphData, SIZE, pheromones, alpha, beta);
         }
-        cout << endl;
+        for (int i = 0; i < ANTS_N; i++) {
+            depositAntPheromone(antPathArray[i], pheromones, SIZE);
+        }
+        evaporatePheromones(pheromones, SIZE);
     }
-    cout << endl;
+    printGraph(graphData, SIZE);
+    printPheromone(pheromones, SIZE);
 
+    // print all of the paths
+    for (int i = 0; i < ANTS_N; i++) {
+        printPath(antPathArray[i]);
+    }
     
-    // print pheromones
-    cout << "pheromones: " << endl;
-    for (int i = 0; i < graphSize; i++) {
-        for (int j = 0; j < graphSize; j++) {
-            cout << pheromones[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    
+
+    // vector<int> neighbors = getNeighbors(2, graphData, graphSize);
+    // vector<int> alreadyVisited;
+    // alreadyVisited.push_back(1);
+    // alreadyVisited.push_back(3);
+    // vector<int> neighborsCleaned = eliminateAlreadyVisitedNeighbors(neighbors, alreadyVisited);
+
+    // // print neighbors vector  
+    // cout << "neighbors: " << endl;
+    // for (int i = 0; i < neighbors.size(); i++) {
+    //     cout << neighbors[i] << endl;
+    // }
+    // cout << endl;
+
+
+    // // print neighborsCleaned vector
+    // cout << "cleaned neighbors: " << endl;
+    // for (int i = 0; i < neighborsCleaned.size(); i++) {
+    //     cout << neighborsCleaned[i] << endl;
+    // }
+    // cout << endl;
+
 
     // calculate probability and print it
-    unordered_map<int, double> probability = calculateProbability(2, graphData, SIZE, pheromones, alpha, beta);
-    cout << "probability: " << endl;
-    for (auto const& x : probability) {
-        cout << x.first << " " << x.second << endl;
-    }
-    cout << endl;
+    // unordered_map<int, double> probability = calculateProbability(2, graphData, SIZE, pheromones, alpha, beta);
+    // cout << "probability: " << endl;
+    // for (auto const& x : probability) {
+    //     cout << x.first << " " << x.second << endl;
+    // }
+    // cout << endl;
 
     // Evaporate pheromones and print it
-    evaporatePheromones(pheromones, graphSize);
-    cout << "evaporated pheromones: " << endl;
-    for (int i = 0; i < graphSize; i++) {
-        for (int j = 0; j < graphSize; j++) {
-            cout << pheromones[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
+    // evaporatePheromones(pheromones, graphSize);
+    // cout << "evaporated pheromones: " << endl;
+    // for (int i = 0; i < graphSize; i++) {
+    //     for (int j = 0; j < graphSize; j++) {
+    //         cout << pheromones[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
 
     // Create a sample antPath
-    AntPath antPath;
-    antPath.path.push_back(1);
-    antPath.path.push_back(2);
-    antPath.path.push_back(3);
-    antPath.path.push_back(4);
-    antPath.path.push_back(1);
-    antPath.pathLength = 10;
-    // Deposit pheromones and print it
-    depositAntPheromone(antPath, pheromones, SIZE);
-    cout << "deposited pheromones: " << endl;
-    for (int i = 0; i < graphSize; i++) {
-        for (int j = 0; j < graphSize; j++) {
-            cout << pheromones[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;    
+    // AntPath antPath;
+    // antPath.path.push_back(1);
+    // antPath.path.push_back(2);
+    // antPath.path.push_back(3);
+    // antPath.path.push_back(4);
+    // antPath.path.push_back(1);
+    // antPath.pathLength = 10;
+    // // Deposit pheromones and print it
+    // depositAntPheromone(antPath, pheromones, SIZE);
+    // cout << "deposited pheromones: " << endl;
+    // for (int i = 0; i < graphSize; i++) {
+    //     for (int j = 0; j < graphSize; j++) {
+    //         cout << pheromones[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
 
     return 0;
 }
