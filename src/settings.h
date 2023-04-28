0a6008c74df3c85185780ac93863ed82030f0845
Mousavi <seyed.mousavi@hpc-head-n1.unitn.it>
2023-04-28 20:51:41 +0200

    Update settings

--- src/settings.h
+++ src/settings.h
@@ -1,7 +1,7 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define SIZE 200
+#define SIZE 500
 #define INDEX(i, j) (i * SIZE + j)
 #define NULL_TIE -1
 #define T_PHER float
@@ -15,17 +15,17 @@
 #define EVAP_RATE 0.1
 #define K_CONST 0.4
 #define PHER_CONST 0.4
-#define ALPHA 0.9
-#define BETA 0.1
+#define ALPHA 0.5
+#define BETA 2.0
 #define SEED 0
 
-#define ANTS_N 8
-#define ANTS_ITER 100
+#define ANTS_N 4
+#define ANTS_ITER 300
 
 // Only used in the parallel version
 #define MPI_T_PHER MPI_FLOAT
 #define MPI_T_GRAPH MPI_FLOAT
-#define COMM_NUM 10
+#define COMM_NUM 30
 // 'W' for updating the worst colony only and 'A' for updating all.
 #define UPDATE_ALL 'A'
 #define UPDATE_WORST 'W'
@@ -35,6 +35,5 @@
 #define M_SEND_PHER 1
 #define M_RECV_PHER 2
 
-#define OUT_DIR string("out")
-
 #endif
+
